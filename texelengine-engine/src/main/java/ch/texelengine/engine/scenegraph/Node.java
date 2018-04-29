10a49dc7143c6169fa9af54ba890cddf57541405
Dorian <dorian.ros@gmail.com>
2018-04-29 19:02:32 +0200

    Added documentation

--- texelengine-engine/src/main/java/ch/texelengine/engine/scenegraph/Node.java
+++ texelengine-engine/src/main/java/ch/texelengine/engine/scenegraph/Node.java
@@ -4,28 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Represent a node in a {@link Scene}
+ *
+ * <p>
+ * This class the base class for every type of node that are
+ * present in the engine.
+ * </p>
  *
  * @author Dorian Ros
  */
 public abstract class Node {
 
     /**
-     *
+     * Parent node of <code>this</code> node
      */
     private Node parent;
 
     /**
-     *
+     * List of child nodes of <code>this</code> node
      */
     private List<Node> children;
 
     /**
-     *
+     * Flag that defines if the node needs to be updated
      */
     private boolean changed;
 
     /**
-     *
+     * Construct a new {@link Node} object
      */
     public Node() {
         this.parent = null;
@@ -34,12 +40,21 @@ public abstract class Node {
     }
 
     /**
+     * Update <code>this</code> node
      *
+     * <p>
+     * This is responsible for updating this node and this node only
+     * </p>
      */
     public abstract void update();
 
     /**
+     * Update <code>this</code> node and all its children
      *
+     * <p>
+     * This function is recursive so a call to this function from a node
+     * automatically updates all of the nodes of the tree below
+     * </p>
      */
     protected void updateAll() {
         update();
@@ -51,7 +66,13 @@ public abstract class Node {
     }
 
     /**
+     * Notify <code>this</code> node and all its children for a state
+     * change
      *
+     * <p>
+     * This function is recursive so a call to this function from a node
+     * automatically notifies all of the nodes of the tree below
+     * </p>
      */
     protected void notifyChanged() {
         this.changed = true;
--- texelengine-engine/src/main/java/ch/texelengine/engine/scenegraph/Scene.java
+++ texelengine-engine/src/main/java/ch/texelengine/engine/scenegraph/Scene.java
@@ -1,26 +1,33 @@
 package ch.texelengine.engine.scenegraph;
 
 /**
+ * Represent a scene containing nodes that can be rendered
+ *
+ * <p>
+ * The scene is defined by its root node onto which can be added children
+ * that form a tree.
+ * </p>
  *
  * @author Dorian Ros
  */
 public class Scene {
 
     /**
-     *
+     * Root {@link Node} for the scene
      */
     private final Node root;
 
     /**
+     * Construct a new {@link Scene} object
      *
-     * @param root
+     * @param root the root {@link Node} for the scene
      */
     public Scene(Node root) {
         this.root = root;
     }
 
     /**
-     *
+     * Update the scene and all the nodes in it
      */
     public void udpate() {
         this.root.updateAll();
