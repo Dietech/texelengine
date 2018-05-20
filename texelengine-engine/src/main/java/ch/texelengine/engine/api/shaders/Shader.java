9922ed1c7d7be284229edd43bd7d42b370d20316
Dorian <dorian.ros@gmail.com>
2018-05-20 17:11:28 +0200

    Added Shader classes

---
+++ texelengine-engine/src/main/java/ch/texelengine/engine/api/shaders/Shader.java
@@ -0,0 +1,8 @@
+package ch.texelengine.engine.api.shaders;
+
+/**
+ * @author Dorian Ros
+ */
+public abstract class Shader {
+
+}
---
+++ texelengine-engine/src/main/java/ch/texelengine/engine/platform/opengl/shaders/GLShader.java
@@ -0,0 +1,10 @@
+package ch.texelengine.engine.platform.opengl.shaders;
+
+import ch.texelengine.engine.api.shaders.Shader;
+
+/**
+ * @author Dorian Ros
+ */
+public class GLShader extends Shader {
+
+}
